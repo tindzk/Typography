284cc517f487c0e21ced5242136c93585fd279d4
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-11-14 17:45:14 +0100

    Move parser and render code to `docslib' repository.

--- build.sh
+++ build.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
-mkdir -p build/{docsgen,Jivai}
+mkdir -p build/{docsgen,docslib,Jivai}
 
 ../tplgen/tplgen.bin       \
 	name=Template          \
 	itf=no                 \
 	add=Main:tpl/Main.html \
-	add=Body:tpl/Body.html \
+	add=Body:../docslib/tpl/Body.html \
 	out=src/Template       \
 	|| exit 1
 
-../Jivai-Utils/jutils.bin        \
-	build                        \
-	output=docsgen.bin           \
-	main=src/Main.c              \
-	manifest=Manifest.h          \
-	include=../Jivai/src         \
-	include=../tplgen/include    \
-	map=src:build/docsgen        \
-	map=../Jivai/src:build/Jivai \
-	optimlevel=0                 \
-	inclhdr=config.h             \
-	dbgsym=yes                   \
+../Jivai-Utils/jutils.bin            \
+	build                            \
+	output=docsgen.bin               \
+	main=src/Main.c                  \
+	manifest=Manifest.h              \
+	include=../Jivai/src             \
+	include=../tplgen/include        \
+	include=../docslib/src           \
+	map=src:build/docsgen            \
+	map=../Jivai/src:build/Jivai     \
+	map=../docslib/src:build/docslib \
+	optimlevel=0                     \
+	inclhdr=config.h                 \
+	dbgsym=yes                       \
 	link=@bfd
--- src/TemplateHelper.h
+++
@@ -1,8 +0,0 @@
-#import <String.h>
-#import <HTML/Entities.h>
-
-#import "Body.h"
-
-#define html(...) HTML_Entities_Encode(__VA_ARGS__)
-
-String TemplateHelper_PrintStyled(int style, String s);
